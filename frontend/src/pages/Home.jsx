4ab28df6a0849fc98ca835c9c3696560779db0c3
jiteshpahwa04 <jiteshofficial2004@gmail.com>
2023-07-17 20:01:11 +0530

    Banner change

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -21,6 +21,22 @@ function Home() {
             <img src="carLogos/toyota.png" alt="car-logo" />
             <img src="carLogos/volvo.png" alt="car-logo" />
             <img src="carLogos/ww.png" alt="car-logo" />
+            
+            <img src="carLogos/bmw.png" alt="car-logo" />
+            <img src="carLogos/ford.png" alt="car-logo" />
+            <img src="carLogos/hundai.png" alt="car-logo" />
+            <img src="carLogos/lamborghini.png" alt="car-logo" />
+            <img src="carLogos/land-rover.png" alt="car-logo" />
+            <img src="carLogos/mclaren.png" alt="car-logo" />
+            <img src="carLogos/merc.png" alt="car-logo" />
+            <img src="carLogos/mini.png" alt="car-logo" />
+            <img src="carLogos/nissan.png" alt="car-logo" />
+            <img src="carLogos/porche.png" alt="car-logo" />
+            <img src="carLogos/renault.png" alt="car-logo" />
+            <img src="carLogos/skoda.png" alt="car-logo" />
+            <img src="carLogos/toyota.png" alt="car-logo" />
+            <img src="carLogos/volvo.png" alt="car-logo" />
+            <img src="carLogos/ww.png" alt="car-logo" />
           </div>
       </div>
     </div>
